145bfaa260f190daf9198531180bfcf3a1792260
RuzannaKusikyan <ruzanna.kusikyan@gmail.com>
2019-08-18 13:10:32 +0400

    a

---
+++ Lecture 1/Practical/program3.py
@@ -0,0 +1,10 @@
+course = "Intro to Python"
+student = "Ruzanna Kusikyan"
+nickname = "Ruz"
+grade = 100
+pythonExpert = True
+print("Course:",course)
+print("Student:",student)
+print("Nickname:",nickname)
+print("Grade:",grade)
+print("PythonExpert:",pythonExpert)
